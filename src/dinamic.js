9ec6dea681a816a1407655b090c12ad339b6fc90
Valdeci de Moura <moura97.valdeci@gmail.com>
2025-01-15 22:17:30 -0300

    feat(app): create divide function

--- src/dinamic.js
+++ src/dinamic.js
@@ -1,3 +1,5 @@
+const divide = require('./divide').divide;
+
 function sumDinamically(numbers) {
   const total = numbers.reduce((acc, curr) => acc + curr, 0);
 
@@ -6,5 +8,6 @@ function sumDinamically(numbers) {
 
 const numbers = [1, 2, 3, 45, 56, 88, 7, 9];
 
+const total = sumDinamically(numbers);
 
-console.log({ total: sumDinamically(numbers) });
+console.log({ total, divisao: divide(total, 11) });
